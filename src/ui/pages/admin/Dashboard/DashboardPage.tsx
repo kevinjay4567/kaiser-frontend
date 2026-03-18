e17214746192ab7f807b32c28e07ebee1aa7d28e
Kevin Jay Herrera <kjayh@unal.edu.co>
2026-03-17 21:50:26 -0500

    fix: eliminacion de funcion redundante en dashboard

--- src/ui/pages/admin/Dashboard/DashboardPage.tsx
+++ src/ui/pages/admin/Dashboard/DashboardPage.tsx
@@ -1,20 +1,6 @@
 import { AdminLayout } from "@/ui/layouts";
-import { useEffect } from "react";
-import { AuthContext } from "@/ui/contexts/auth/AuthContext";
 
 export function DashboardPage() {
-  const context = AuthContext();
-
-  useEffect(() => {
-    context
-      .refreshSession()
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
   return (
     <AdminLayout>
       <h1 className="text-2xl font-bold">
